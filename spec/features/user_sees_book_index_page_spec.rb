97246a0b938c05dc13140932b4f96049641c8743
Tony Robertson <tonyr729@gmail.com>
2018-12-04 18:08:15 -0700

    Add feature test for book index page

---
+++ spec/features/user_sees_book_index_page_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'As a Visitor' do
+  describe 'when I visit a book index page' do
+    it 'displays all book titles' do
+      book_1 = Book.create(title: "Fire and Ice", pages: 400, publication_year: 2016)
+      book_2 = Book.create(title: "Ooo Dragons", pages: 500, publication_year: 2017)
+      book_3 = Book.create(title: "Oh no, ice zombies", pages: 600, publication_year: 2018)
+
+      visit books_path
+  
+      expect(page).to have_content("All Books")
+      expect(page).to have_content(book_1.title)
+      expect(page).to have_content("Pages: #{book_1.pages}")
+      expect(page).to have_content(book_2.title)
+      expect(page).to have_content("Pages: #{book_2.pages}")
+    end
+  end
+end
