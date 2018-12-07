3e69450691a7e3894b123faf8427d8e6c3ed7cad
abenetka <ali.benetka@gmail.com>
2018-12-06 20:26:07 -0700

    Add spec for book delete from show page

--- spec/features/user_sees_book_show_page_spec.rb
+++ spec/features/user_sees_book_show_page_spec.rb
@@ -6,11 +6,11 @@ describe 'As a Visitor' do
       book_1 = Book.create(title: "Fire and Ice", pages: 400, publication_year: 2016)
       book_2 = Book.create(title: "Dragon Zombies", pages: 500, publication_year: 2019)
       author_1 = book_1.authors.create(name: "George Martin")
-      author_2 = book_1.authors.create(name: "HBO")
+      author_2 = book_2.authors.create(name: "HBO")
       user_1 = User.create(name: "Hamburglar")
       review_1 = user_1.reviews.create(title: "Terrible book!", description: "Bitters retro mustache aesthetic biodiesel 8-bit.", rating: 1, book: book_1)
       user_2 = User.create(name: "McDonald")
-      review_2 = user_2.reviews.create(title: "Amazing book!", description: "Park iphone leggings put a bird on it.", rating: 5, book: book_1)
+      review_2 = user_2.reviews.create(title: "Amazing book!", description: "Park iphone leggings put a bird on it.", rating: 5, book: book_2)
 
       visit book_path(book_1)
 
