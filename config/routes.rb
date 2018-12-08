6e77b8082ed1832d5873bf79f734819582d5bb8c
Tony Robertson <tonyr729@gmail.com>
2018-12-08 16:05:26 -0700

    Add implementation for user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :books, only: [:index, :show, :new, :create, :destroy] do
-    resources :reviews, only: [:new, :create]
+  resources :books, shallow: true, only: [:index, :show, :new, :create, :destroy] do
+    resources :reviews, only: [:new, :create] do
+      resources :users, only: [:show]
+    end
   end
 	resources :authors, only: [:show]
 end
