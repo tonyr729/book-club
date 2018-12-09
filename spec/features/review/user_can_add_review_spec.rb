6c37b968d47bbdd2e30ac7aeb1faa2daa4b50273
Tony Robertson <tonyr729@gmail.com>
2018-12-08 18:48:31 -0700

    Add change to add review spec; adds review to existing user if present

--- spec/features/review/user_can_add_review_spec.rb
+++ spec/features/review/user_can_add_review_spec.rb
@@ -26,19 +26,19 @@ describe 'As a visitor' do
       expect(page).to have_content(review_title)
     end
 
-    it 'displays error if user name is not unique' do
+    it 'adds review to existing reviews if user already exists' do
       book = Book.create(title: "Ooo Dragons", pages: 500, publication_year: 2017)
 
       visit new_book_review_path(book)
 
-      review_title = "Book is fire"
-      review_description = "Dragons are lit"
+      review_1_title = "Book is fire"
+      review_1_description = "Dragons are lit"
       review_user = "Puff"
-      review_rating = 'Great'
+      review_1_rating = 'Horrible'
 
-      fill_in :review_title, with: review_title
-      fill_in :review_description, with: review_description
-      select review_rating
+      fill_in :review_title, with: review_1_title
+      fill_in :review_description, with: review_1_description
+      select review_1_rating
       fill_in :review_user, with: review_user
 
       click_button "Create Review"
@@ -47,19 +47,24 @@ describe 'As a visitor' do
 
       visit new_book_review_path(book)
 
-      review_title = "Book is really good"
-      review_description = "Dragons are fire"
-      review_user = "Puff"
-      review_rating = 'Great'
+      review_2_title = "Book is really good"
+      review_2_description = "Dragons are fire"
+      review_2_rating = 'Great'
 
-      fill_in :review_title, with: review_title
-      fill_in :review_description, with: review_description
-      select review_rating
+      fill_in :review_title, with: review_2_title
+      fill_in :review_description, with: review_2_description
+      select review_2_rating
       fill_in :review_user, with: review_user
 
       click_button "Create Review"
 
-      expect(page).to have_content("Name has already been taken")
+      user_1 = User.find_by(name: review_user)
+
+      visit user_path(user_1)
+
+      expect(page).to have_content(review_user)
+      expect(page).to have_content(review_1_title)
+      expect(page).to have_content(review_2_title)
     end
   end
 end
