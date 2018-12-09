c5b793f2082bb2746b8485628cc7bb2cb4993fd5
abenetka <ali.benetka@gmail.com>
2018-12-09 11:51:17 -0700

    Adds implementation of delete author

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@ class Author < ApplicationRecord
   validates_presence_of :name
   validates :name, uniqueness: true
 
-  has_many :book_authors
+  has_many :book_authors, :dependent => :destroy
   has_many :books, through: :book_authors
 
 
