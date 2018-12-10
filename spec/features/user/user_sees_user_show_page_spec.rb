f6435795f06a20263dec9aaa94fb5a93ab5642b0
Tony Robertson <tonyr729@gmail.com>
2018-12-10 13:46:53 -0700

    Add feature spec for user seeing reviews chronologically

--- spec/features/user/user_sees_user_show_page_spec.rb
+++ spec/features/user/user_sees_user_show_page_spec.rb
@@ -25,5 +25,28 @@ describe 'As a Visitor' do
       expect(page).to have_content("Description: #{review_2.description}")
       expect(page).to have_content("Rating: #{review_2.rating}")
     end
+
+    it 'can sort reviews chronologically' do
+      book_1 = Book.create(title: "Fire and Ice", pages: 400, publication_year: 2016)
+      book_2 = Book.create(title: "Dragon Zombies", pages: 500, publication_year: 2019)
+      book_3 = Book.create(title: "Zombie Dragon love story", pages: 500, publication_year: 2019)
+      user_1 = User.create(name: "Hamburglar")
+      review_1 = user_1.reviews.create(title: "Terrible book!", description: "Bitters retro mustache aesthetic biodiesel 8-bit.", rating: 1, book: book_1, created_at: 1.day.ago)
+      review_2 = user_1.reviews.create(title: "Amazing book!", description: "Park iphone leggings put a bird on it.", rating: 5, book: book_2)
+      review_3 = user_1.reviews.create(title: "It was a book!", description: "I want my money back", rating: 2, book: book_3, created_at: 1.day.from_now)
+
+      visit user_path(user_1)
+
+      click_link "Newest"
+      
+      within "#review_title_0" do
+        expect(page).to have_content(review_3.title)
+      end
+
+      click_link "Oldest"
+      within "#review_title_0" do
+        expect(page).to have_content(review_1.title)
+      end
+    end
   end
 end
